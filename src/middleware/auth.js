83e9ffd20b744773df1cf5a7bd1087d846d8ade4
plougsgaard <thomas@plougsgaard.dk>
2016-09-02 18:35:03 +0200

    Add body to 401.
    JSON must be passed most-always.

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,15 @@
+import Boom from 'boom'
 import { getSession } from '../controllers/auth'
 
 /**
  * Protect resources that require authentication.
- * 
+ *
  * Depends on the `sessionMiddleware` having the `session` prior to this.
  */
 const authMiddleware = async (ctx, next) => {
   const { session } = ctx
   if (!session) {
+    ctx.body = Boom.unauthorized('(╯°□°）╯︵ ┻━┻')
     ctx.status = 401
   } else {
     return await next()
