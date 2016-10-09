3b6e90db854db600f73d52035de36938a89b6a47
plougsgaard <thomas@plougsgaard.dk>
2016-10-09 13:47:50 +0200

    Read AWS keys.

--- src/config.js
+++ src/config.js
@@ -4,3 +4,5 @@ export const DB_NAME = process.env.DB_NAME || 'hamwich'
 export const DB_PORT = process.env.DB_PORT || 5432
 export const DB_USER = process.env.DB_USER || 'development'
 export const DB_PASS = process.env.DB_PASS || 'development'
+export const AWS_KEY = process.env.AWS_KEY
+export const AWS_SECRET = process.env.AWS_SECRET
