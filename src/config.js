f716b5882fee929acd873999f7080111d259996f
plougsgaard <thomas@plougsgaard.dk>
2016-10-09 20:59:41 +0200

    Also load the region.

--- src/config.js
+++ src/config.js
@@ -6,4 +6,5 @@ export const DB_USER = process.env.DB_USER || 'development'
 export const DB_PASS = process.env.DB_PASS || 'development'
 export const AWS_KEY = process.env.AWS_KEY
 export const AWS_SECRET = process.env.AWS_SECRET
+export const AWS_REGION = process.env.AWS_REGION
 export const AWS_BUCKET = process.env.AWS_BUCKET
